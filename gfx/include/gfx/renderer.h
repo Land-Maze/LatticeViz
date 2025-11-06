c54c4277806241177cf4672c34e430b7b0a156ec
Timur Dzhesur <61432929+Land-Maze@users.noreply.github.com>
2025-11-07 00:45:09 +0100

    feat: add OpenGL initialization

--- gfx/include/gfx/renderer.h
+++ gfx/include/gfx/renderer.h
@@ -1,10 +1,17 @@
 #pragma once
 
-#include <webgpu/wgpu.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 
 namespace LatticeViz::GFX
 {
 	class Renderer
 	{
+	public:
+		Renderer() = default;
+		~Renderer() = default;
+
+		void Initialize(GLFWwindow* window);
+	private:
 	};
 }
--- gfx/src/renderer.cpp
+++ gfx/src/renderer.cpp
@@ -1,5 +1,19 @@
-#include <gfx/Renderer.h>
+#include <iostream>
+#include <gfx/renderer.h>
+#include <glad/glad.h>
 
 namespace LatticeViz::GFX
 {
+	void Renderer::Initialize(GLFWwindow* window)
+	{
+		glfwMakeContextCurrent(window);
+
+		if(!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+		{
+			std::cerr << "Failed to initialize GLAD\n";
+			return;
+		}
+
+		std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
+	}
 }
