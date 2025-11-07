7e35351ef25d66475371b6807149f22492c9745b
Land_Maze <61432929+Land-Maze@users.noreply.github.com>
2025-11-07 08:34:40 +0100

    feat: add GLM dependency

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -46,3 +46,14 @@ add_library(glad STATIC
 target_include_directories(glad PUBLIC
 		third-party/glad/include/
 )
+
+# ---------------------------------------------------------------------------
+# GLM
+# ---------------------------------------------------------------------------
+FetchContent_Declare(
+		glm
+		GIT_REPOSITORY https://github.com/g-truc/glm.git
+		GIT_TAG 1.0.2
+		GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(glm)
